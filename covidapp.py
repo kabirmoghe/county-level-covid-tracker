dcb69576d4cbcf671ff0babce37396e9ae0c5663
kabirmoghe <kabirmoghe@gmail.com>
2021-05-30 21:18:03 -0400

    added path

--- covidapp.py
+++ covidapp.py
@@ -659,11 +659,11 @@ def county_stats(county_name):
             return "Please enter a valid county name (i.e. Orange County, CA). The county you entered, '{county_name}', may not have complete information.".format(county_name = county_name)
 
 def usplot():
-    '''with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
         counties = json.load(response)
-    '''
+    
     data = create_covid_pop_data()
-    '''
+    
     last_inf_rate = [column for column in data.columns if "Infection" in column.split() and "Cumulative" not in column.split() and "Predicted" not in column.split()][-1]
 
     data['Log {name}'.format(name = last_inf_rate)] = data[last_inf_rate].apply(lambda value: np.log(value) if value != 0 else value)
@@ -677,8 +677,8 @@ def usplot():
                           )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.update_traces(marker_line_width=0)
-    fig.write_html("../covidapp/templates/usplot.html", full_html = False)
-    '''
+    fig.write_html("/app/templates/usplot.html", full_html = False)
+    
     cols = data.columns
 
     inf_col = 0

d852ebcadadf6223b663a277ac4b5172117dfb82
kabirmoghe <kabirmoghe@gmail.com>
2021-06-23 01:10:09 -0400

    added vaxx protocol

--- vaxx.py
+++ vaxx.py
@@ -44,68 +44,70 @@ def create_vaxx_data():
 '''
 
 def create_vaxx_data():
-    
-    aws_access_key_id = 'AKIA2RCGHBDMFYOMRPOY'
-    aws_secret_access_key = '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'
-    
-    data = pd.read_csv('https://data.cdc.gov/api/views/8xkx-amqh/rows.csv?accessType=DOWNLOAD')
-    
-    no_mo = {1:'January',
-             2:'February',
-             3:'March',
-             4:'April',
-             5:'May',
-             6:'June',
-             7:'July',
-             8:'August',
-             9:'September',
-             10:'October',
-             11:'November',
-             12:'December'
-         }
-    
-    latest_date = data['Date'][0]
-    def word_name(date):
-        month, day, year = [int(value) for value in date.split('/')]
-        date = '{month} {day}, {year}'.format(month = no_mo[month], day = day, year = year)
+    def data():
+        data = pd.read_csv('https://data.cdc.gov/api/views/8xkx-amqh/rows.csv?accessType=DOWNLOAD')
+
+        no_mo = {1:'January',
+                 2:'February',
+                 3:'March',
+                 4:'April',
+                 5:'May',
+                 6:'June',
+                 7:'July',
+                 8:'August',
+                 9:'September',
+                 10:'October',
+                 11:'November',
+                 12:'December'
+             }
+
+        latest_date = data['Date'][0]
+
+        def word_name(date):
+            month, day, year = [int(value) for value in date.split('/')]
+            date = '{month} {day}, {year}'.format(month = no_mo[month], day = day, year = year)
+
+            return date
+
+        date = word_name(latest_date)
+
+        data['Recip_County'] = data['Recip_County'] + ', ' + data['Recip_State']
+
+        dates = list(data['Date'])
+
+        def get_months():
+
+            m_dates = []
+
+            l_month = 0
+
+            for date in dates:
+                month = int(date.split('/')[0])
+                if month != l_month:
+                    m_dates.append(date)
+                l_month = month
+
+            return m_dates
+
+        data = data[data['Date'].isin(get_months())]
+
+        for col in data.columns:
+            if col != 'Recip_County' and 'Pct' not in col and col != 'Date':
+                data.drop(col, axis = 1, inplace = True)
+
+        data.columns = ['Date', 'County Name', '% Fully Vaccinated as of {}'.format(date),'% ≥ 12 Fully Vaccinated as of {}'.format(date), '% ≥ 18 Fully Vaccinated as of {}'.format(date), '% ≥ 65 Fully Vaccinated as of {}'.format(date)]
+
+        dates = list(data['Date'])
+
+        data = data.iloc[::-1]
+
+        data.reset_index(drop = True, inplace = True)
+
+        data['Date'] = data['Date'].apply(lambda date: word_name(date))
         
-        return date
+        return data
     
-    date = word_name(latest_date)
-    
-    data['Recip_County'] = data['Recip_County'] + ', ' + data['Recip_State']
-        
-    dates = list(data['Date'])
-
-    def get_months():
-        
-        m_dates = []
-
-        l_month = 0
-
-        for date in dates:
-            month = int(date.split('/')[0])
-            if month != l_month:
-                m_dates.append(date)
-            l_month = month
-            
-        return m_dates
-    
-    data = data[data['Date'].isin(get_months())]
-    
-    for col in data.columns:
-        if col != 'Recip_County' and 'Pct' not in col and col != 'Date':
-            data.drop(col, axis = 1, inplace = True)
-        
-    data.columns = ['Date', 'County Name', '% Fully Vaccinated as of {}'.format(date),'% ≥ 12 Fully Vaccinated as of {}'.format(date), '% ≥ 18 Fully Vaccinated as of {}'.format(date), '% ≥ 65 Fully Vaccinated as of {}'.format(date)]
-
-    data = data.iloc[::-1]
-    
-    data.reset_index(drop = True, inplace = True)
-    
-    data['Date'] = data['Date'].apply(lambda date: word_name(date))
-
-    #data.to_csv('vaxxdataset.csv')
+    data = data()
     
     filename = 'vaxxdataset.csv'
     bucketname = 'coviddatakm'
@@ -116,6 +118,6 @@ def create_vaxx_data():
     client = boto3.client('s3')
     
     response = client.put_object(Body = csv_buffer.getvalue(), Bucket = bucketname, Key = filename)
-
+    
 if __name__ == '__main__':
     create_vaxx_data()
